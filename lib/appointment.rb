a7fa3043aa46e1bafba3d2bf8386b42aaa18c024
Bobby Neal <rsneal01@gmail.com>
2020-06-22 16:49:56 +0000

    Automatically backed up by Learn

---
+++ lib/appointment.rb
@@ -0,0 +1,20 @@
+class Appointment
+  
+  attr_accessor :date, :patient, :doctor
+  
+  @@all = []
+  
+  def initialize(date, patient, doctor)
+    @date = date
+    @patient = patient
+    @doctor = doctor
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def patient
+    
+  end
+end
